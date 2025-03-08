a53a2e9ac0218339be5585a8366d5fdeed9aaa5a
Luis <58019574+ludev84@users.noreply.github.com>
2025-03-08 03:04:55 -0600

    Simplify App structure

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,9 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import AppTest from './App-test.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <AppTest />
+    <App />
   </StrictMode>,
 )
