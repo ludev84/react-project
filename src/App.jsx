aa992df1651895adde9419ddd5a85ecd3b41335c
Luis <58019574+ludev84@users.noreply.github.com>
2025-03-09 19:19:39 -0600

    Forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,24 @@
 import React from "react"
-import avatar from "./assets/user.png"
-import starFilled from "./assets/star-filled.png"
-import starEmpty from "./assets/star-empty.png"
 
 export default function App() {
-    const [contact, setContact] = React.useState({
-        firstName: "John",
-        lastName: "Doe",
-        phone: "+1 (212) 555-1212",
-        email: "itsmyrealname@example.com",
-        isFavorite: false
-    })
-    
-    function toggleFavorite() {
-        setContact(prevContact => ({
-                ...prevContact,
-                isFavorite: !prevContact.isFavorite
-            }))
+
+    function signUp(formData) {
+        const email = formData.get("email")
+        const password = formData.get("password")
+        console.log(password)
     }
 
-    let starIcon = contact.isFavorite ? starFilled : starEmpty
-
     return (
-        <main>
-            <article className="card">
-                <img
-                    src={avatar}
-                    className="avatar"
-                    alt="User profile picture of John Doe"
-                />
-                <div className="info">
-                    <button
-                        onClick={toggleFavorite}
-                        aria-pressed={contact.isFavorite}
-                        aria-label={contact.isFavorite ? "Remove from favorites" : "Add to favorites"}
-                        className="favorite-button"
-                    >
-                        <img
-                            src={starIcon}
-                            alt={contact.isFavorite ? "filled star icon" : "empty star icon"}
-                            className="favorite"
-                        />
-                    </button>
-                    <h2 className="name">
-                        {contact.firstName} {contact.lastName}
-                    </h2>
-                    <p className="contact">{contact.phone}</p>
-                    <p className="contact">{contact.email}</p>
-                </div>
-
-            </article>
-        </main>
+        <section>
+          <h1>Signup form</h1>
+          <form action={signUp}>
+            <label htmlFor="email">Email:</label>
+            <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
+            <br />
+            <label htmlFor="password">Password</label>
+            <input id="password" type="password" name="password" />
+            <button>Submit</button>
+          </form>
+        </section>
     )
 }
